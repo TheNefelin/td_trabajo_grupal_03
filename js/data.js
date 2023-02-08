0a66e1b7865147ae1902bb02802ac07979a01ca0
Andrea Alvarez <andreaelenaalvarez2019@gmail.com>
2023-02-07 21:39:28 -0300

    creo rama andrea

---
+++ js/data.js
@@ -0,0 +1,13 @@
+var productos;
+
+productos=[
+{
+    nombre:"Age of Empires IV PC (DIGITAL)",
+    imagen:"img/JUE2119D_2022112414538_f.jpg",
+    precio:29.990,
+    descripcion:,
+    plataforma:"PC",
+
+
+},
+]
